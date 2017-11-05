4c2946778d3cf1a1f29049aa342f32fbf7473244
Thomas Sjolshagen <thomas@eighty20results.com>
2017-11-05 05:25:05 -0700

    ENHANCEMENT: Load localization function(s) ENHANCEMENT: Use plugin slug (for standard version) in localization ENHANCEMENT: Fixed code style ENHANCEMENT: Explicit priority for all action hooks

--- pmpro-extended-membership-directory.php
+++ pmpro-extended-membership-directory.php
@@ -3,7 +3,7 @@
 Plugin Name: Extended Membership Directory for Paid Memberships Pro (Add-on)
 Plugin URI: https://eighty20results.com/wordpress-plugins/pmpro-extended-membership-directory
 Description: Extended version of the PMPro Membership Directory add-on
-Version: 1.4
+Version: 1.5
 Author: eighty20results, strangerstudios
 Author URI: https://eighty20results.com/thomas-sjolshagen
 Text Domain: pmpro-extended-membership-directory
@@ -14,120 +14,157 @@ License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
 global $pmpromd_options;
 
-if ( ! defined("PMPRO_EXTENDED_DIRECTORY" ) ) {
-    define('PMPRO_EXTENDED_DIRECTORY', true );
+if ( ! defined( "PMPRO_EXTENDED_DIRECTORY" ) ) {
+	define( 'PMPRO_EXTENDED_DIRECTORY', true );
 }
 
-if ( ! defined("PMPROED_VER" ) ) {
-	define('PMPROED_VER', "1.4" );
+if ( ! defined( "PMPROED_VER" ) ) {
+	define( 'PMPROED_VER', "1.5" );
 }
 
 
-$path = dirname(__FILE__);
-$custom_dir = get_stylesheet_directory()."/paid-memberships-pro/pmpro-member-directory/";
-$custom_directory_file = $custom_dir."directory.php";
-$custom_profile_file = $custom_dir."profile.php";
+$path                  = dirname( __FILE__ );
+$custom_dir            = get_stylesheet_directory() . "/paid-memberships-pro/pmpro-member-directory/";
+$custom_directory_file = $custom_dir . "directory.php";
+$custom_profile_file   = $custom_dir . "profile.php";
 
 //load custom or default templates
-if(file_exists($custom_directory_file))
-	require_once($custom_directory_file);	
-else
-	require_once($path . "/templates/directory.php");
-if(file_exists($custom_profile_file))
-	require_once($custom_profile_file);	
-else
-	require_once($path . "/templates/profile.php");
+if ( file_exists( $custom_directory_file ) ) {
+	require_once( $custom_directory_file );
+} else {
+	require_once( $path . "/templates/directory.php" );
+}
+if ( file_exists( $custom_profile_file ) ) {
+	require_once( $custom_profile_file );
+} else {
+	require_once( $path . "/templates/profile.php" );
+}
+
+// Add localization feature(s)
+require_once( "{$path}/includes/localization.php");
 
 function pmpromd_register_styles() {
 	//load stylesheet (check child theme, then parent theme, then plugin folder)	
-	if(file_exists(get_stylesheet_directory()."/paid-memberships-pro/member-directory/css/pmpro-member-directory.css"))
-		wp_register_style( 'pmpro-member-directory-styles', get_stylesheet_directory_uri()."/paid-memberships-pro/member-directory/css/pmpro-member-directory.css", NULL, PMPROED_VER );
-	elseif(file_exists(get_template_directory()."/paid-memberships-pro/member-directory/css/pmpro-member-directory.css"))
-		wp_register_style( 'pmpro-member-directory-styles', get_template_directory_uri()."/paid-memberships-pro/member-directory/css/pmpro-member-directory.css", NULL, PMPROED_VER );
-	elseif(function_exists("pmpro_https_filter"))
-		wp_register_style( 'pmpro-member-directory-styles', pmpro_https_filter(plugins_url( 'css/pmpro-member-directory.css', __FILE__ ) ), NULL, PMPROED_VER );
-	else
-		wp_register_style( 'pmpro-member-directory-styles', plugins_url( 'css/pmpro-member-directory.css', __FILE__ ), NULL, PMPROED_VER );
-
+	if ( file_exists( get_stylesheet_directory() . "/paid-memberships-pro/member-directory/css/pmpro-member-directory.css" ) ) {
+		wp_register_style( 'pmpro-member-directory-styles', get_stylesheet_directory_uri() . "/paid-memberships-pro/member-directory/css/pmpro-member-directory.css", null, PMPROED_VER );
+	} else if ( file_exists( get_template_directory() . "/paid-memberships-pro/member-directory/css/pmpro-member-directory.css" ) ) {
+		wp_register_style( 'pmpro-member-directory-styles', get_template_directory_uri() . "/paid-memberships-pro/member-directory/css/pmpro-member-directory.css", null, PMPROED_VER );
+	} else if ( function_exists( "pmpro_https_filter" ) ) {
+		wp_register_style( 'pmpro-member-directory-styles', pmpro_https_filter( plugins_url( 'css/pmpro-member-directory.css', __FILE__ ) ), null, PMPROED_VER );
+	} else {
+		wp_register_style( 'pmpro-member-directory-styles', plugins_url( 'css/pmpro-member-directory.css', __FILE__ ), null, PMPROED_VER );
+	}
+	
 	wp_enqueue_style( 'pmpro-member-directory-styles' );
 }
-add_action( 'wp_enqueue_scripts', 'pmpromd_register_styles' );
+add_action( 'wp_enqueue_scripts', 'pmpromd_register_styles', 10 );
 
-function pmpromd_extra_page_settings($pages) {
-   $pages['directory'] = array('title'=>'Directory', 'content'=>'[pmpro_member_directory]', 'hint'=>'Include the shortcode [pmpro_member_directory].');
-   $pages['profile'] = array('title'=>'Profile', 'content'=>'[pmpro_member_profile]', 'hint'=>'Include the shortcode [pmpro_member_profile].');
-   return $pages;
+function pmpromd_extra_page_settings( $pages ) {
+	$pages['directory'] = array(
+		'title'   => __( 'Directory', 'pmpro-membership-directory' ),
+		'content' => '[pmpro_member_directory]',
+		'hint'    => __( 'Include the shortcode [pmpro_member_directory].', 'pmpro-membership-directory' ),
+	);
+	$pages['profile']   = array(
+		'title'   => __( 'Profile', 'pmpro-membership-directory' ),
+		'content' => '[pmpro_member_profile]',
+		'hint'    => __( 'Include the shortcode [pmpro_member_profile].', 'pmpro-membership-directory' ),
+	);
+	
+	return $pages;
 }
-add_action('pmpro_extra_page_settings', 'pmpromd_extra_page_settings');
+
+add_action( 'pmpro_extra_page_settings', 'pmpromd_extra_page_settings', 10 );
 
 //show the option to hide from directory on edit user profile
-function pmpromd_show_extra_profile_fields($user)
-{
-	global $pmpro_pages;	
-?>
-	<h3><?php echo get_the_title($pmpro_pages['directory']); ?></h3>
-	<table class="form-table"> 
-		<tbody>
-			<tr class="user-hide-directory-wrap">
-				<th scope="row"></th>
-				<td>
-					<label for="hide_directory">
-						<input name="hide_directory" type="checkbox" id="hide_directory" <?php checked( get_user_meta($user->ID, 'pmpromd_hide_directory', true), 1 ); ?> value="1"><?php printf(__('Hide from %s?','pmpromd'), get_the_title($pmpro_pages['directory']) ); ?>
-					</label>
-				</td>
-			</tr>
-		</tbody>
-	</table>
-<?php
+function pmpromd_show_extra_profile_fields( $user ) {
+	global $pmpro_pages;
+	?>
+    <h3><?php echo get_the_title( $pmpro_pages['directory'] ); ?></h3>
+    <table class="form-table">
+        <tbody>
+        <tr class="user-hide-directory-wrap">
+            <th scope="row"></th>
+            <td>
+                <label for="hide_directory">
+                    <input name="hide_directory" type="checkbox" id="hide_directory" <?php checked( get_user_meta( $user->ID, 'pmpromd_hide_directory', true ), 1 ); ?> value="1"><?php printf( __( 'Hide from %s?', 'pmpro-membership-directory' ), get_the_title( $pmpro_pages['directory'] ) ); ?>
+                </label>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+	<?php
 }
-add_action( 'show_user_profile', 'pmpromd_show_extra_profile_fields' );
-add_action( 'edit_user_profile', 'pmpromd_show_extra_profile_fields' );
- 
-function pmpromd_save_extra_profile_fields( $user_id ) 
-{
-	if ( !current_user_can( 'edit_user', $user_id ) )
-		return false;
 
-	if (isset($_POST['hide_directory']))
+add_action( 'show_user_profile', 'pmpromd_show_extra_profile_fields', 10 );
+add_action( 'edit_user_profile', 'pmpromd_show_extra_profile_fields', 10 );
+
+function pmpromd_save_extra_profile_fields( $user_id ) {
+	if ( ! current_user_can( 'edit_user', $user_id ) ) {
+		return false;
+	}
+	
+	if ( isset( $_POST['hide_directory'] ) ) {
 		update_user_meta( $user_id, 'pmpromd_hide_directory', true );
+	}
 }
-add_action( 'personal_options_update', 'pmpromd_save_extra_profile_fields' );
-add_action( 'edit_user_profile_update', 'pmpromd_save_extra_profile_fields' );
- 
- 
-function pmpromd_display_file_field($meta_field) {
-	$meta_field_file_type = wp_check_filetype($meta_field['fullurl']);
-	switch ($meta_field_file_type['type']) {
+
+add_action( 'personal_options_update', 'pmpromd_save_extra_profile_fields', 10 );
+add_action( 'edit_user_profile_update', 'pmpromd_save_extra_profile_fields', 10 );
+
+
+function pmpromd_display_file_field( $meta_field ) {
+	$meta_field_file_type = wp_check_filetype( $meta_field['fullurl'] );
+	switch ( $meta_field_file_type['type'] ) {
 		case 'image/jpeg':
 		case 'image/png':
 		case 'image/gif':
-			return '<a href="' . $meta_field['fullurl'] . '" title="' . $meta_field['filename'] . '" target="_blank"><img class="subtype-' . $meta_field_file_type['ext'] . '" src="' . $meta_field['fullurl'] . '"><br />' . $meta_field['filename'] . '</a>'; break;
-	case 'video/mpeg':
-	case 'video/mp4': 
-		return do_shortcode('[video src="' . $meta_field['fullurl'] . '"]'); break;
-	case 'audio/mpeg':
-	case 'audio/wav': 
-		return do_shortcode('[audio src="' . $meta_field['fullurl'] . '"]'); break;		
-	default:
-		return '<a href="' . $meta_field['fullurl'] . '" title="' . $meta_field['filename'] . '" target="_blank"><img class="subtype-' . $meta_field_file_type['ext'] . '" src="' . wp_mime_type_icon($meta_field_file_type['type']) . '"><br />' . $meta_field['filename'] . '</a>'; break;
+			return sprintf(
+				'<a href="%s" title="%s" target="_blank"><img class="subtype-%s" src="%s"><br />%s</a>',
+				$meta_field['fullurl'],
+				$meta_field['filename'],
+				$meta_field_file_type['ext'],
+				$meta_field['fullurl'],
+				$meta_field['filename']
+			);
+			break;
+		case 'video/mpeg':
+		case 'video/mp4':
+			return do_shortcode( sprintf( '[video src="%s"]', $meta_field['fullurl'] ) );
+			break;
+		case 'audio/mpeg':
+		case 'audio/wav':
+			return do_shortcode( sprintf( '[audio src="%s"]', $meta_field['fullurl'] ) );
+			break;
+		default:
+			return sprintf(
+				'<a href="%s" title="%s" target="_blank"><img class="subtype-%s" src="%s"><br />%s</a>',
+				$meta_field['fullurl'],
+                $meta_field['filename'],
+                $meta_field_file_type['ext'],
+                wp_mime_type_icon( $meta_field_file_type['type'] ),
+				$meta_field['filename']
+			);
+			break;
 	}
 }
 
 /*
 Function to add links to the plugin row meta
 */
-function pmpromd_plugin_row_meta($links, $file) {
-	if(strpos($file, 'pmpro-member-directory.php') !== false)
-	{
+function pmpromd_plugin_row_meta( $links, $file ) {
+	if ( strpos( $file, 'pmpro-extended-member-directory.php' ) !== false ) {
 		$new_links = array(
-			'<a href="' . esc_url('http://www.paidmembershipspro.com/add-ons/plugins-on-github/pmpro-member-directory/')  . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
-			'<a href="' . esc_url('http://paidmembershipspro.com/support/') . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
+			sprintf( '<a href="%s" title="%s">%s</a>', esc_url( 'http://www.paidmembershipspro.com/add-ons/plugins-on-github/pmpro-member-directory/' ),__( 'View Documentation', 'paid-memberships-pro' ), __( 'Docs', 'paid-memberships-pro' )  ),
+			sprintf( '<a href="%s" title="%s">%s</a>',esc_url( 'http://paidmembershipspro.com/support/' ),__( 'Visit Customer Support Forum', 'pmpro' ),  __( 'Support', 'paid-memberships-pro' ) ),
 		);
-		$links = array_merge($links, $new_links);
+		$links     = array_merge( $links, $new_links );
 	}
+	
 	return $links;
 }
-add_filter('plugin_row_meta', 'pmpromd_plugin_row_meta', 10, 2);
+
+add_filter( 'plugin_row_meta', 'pmpromd_plugin_row_meta', 10, 2 );
 
 if ( ! class_exists( '\\PucFactory' ) ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'plugin-updates/plugin-update-checker.php' );
